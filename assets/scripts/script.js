bd74166cbae543bb22a612d1f8d7fc926d17247c
David Kanwisher <kanwisher@gmail.com>
2017-01-30 14:01:36 -0500

    Clean up style.js, change var to let, add objects, add comments

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,37 +1,60 @@
 
-const myData = JSON.parse(data); //turns data into a JSON object
+$(function() { //on READY!
 
-// Initialize Firebase
-var config = {
+
+
+
+
+//global JSON
+const myData = JSON.parse(data); //turns data string into a JSON object
+
+
+
+
+
+
+
+
+//~~~~~~~~~~~~~~~~~FIREBASE~~~~~~~~~~~~~~~~~~~~~~//
+
+
+
+
+
+
+
+let config = {
   apiKey: "AIzaSyAh7Te4u5BaeLXYSYp78aH4ml0pZOGHOPI",
   authDomain: "clunkerconnectio-1485387596372.firebaseapp.com",
   databaseURL: "https://clunkerconnectio-1485387596372.firebaseio.com",
   storageBucket: "clunkerconnectio-1485387596372.appspot.com",
   messagingSenderId: "1017250549242"
 };
+
+
 firebase.initializeApp(config);
 
-var dataRef = firebase.database();
+let dataRef = firebase.database();
 
-// main functions --------------------------------->
 
-// Capture Button Click
+
+
 $("#searchButton").on("click", function(event) {
   event.preventDefault();
 
-  // YOUR TASK!!!
-  // Code in the logic for storing and retrieving the most recent user.
-  // Don't forget to provide initial data to your Firebase database.
-  var year = $("#yearDataList").val().trim()
-  console.log(year)
-  var make = myData[$("#carMakesDataList").val().trim()].title;
-  console.log(make)
-  var model = $("#carModelsDataList").val().trim();
-  console.log(model)
-  var zip = $("#zip").val().trim();
-  console.log(zip)
-  var prices = $("#prices").val();
-  console.log(prices)
+
+  // Code for storing and retrieving the most recent user.
+
+  let year = $("#yearDataList").val().trim()
+
+  let make = myData[$("#carMakesDataList").val().trim()].title;
+
+  let model = $("#carModelsDataList").val().trim();
+
+  let zip = $("#zip").val().trim();
+
+  let prices = $("#prices").val();
+
 
   // Code for the push
   dataRef.ref().push({
@@ -41,19 +64,11 @@ $("#searchButton").on("click", function(event) {
       model: model,
       zip: zip,
       prices: prices,
-      // dateAdded: firebase.database.ServerValue.TIMESTAMP
+
   });
 });
 
 dataRef.ref().on("child_added", function(childSnapshot) {
-
-      // Log everything that's coming out of snapshot
-      console.log(childSnapshot.val().year);
-      console.log(childSnapshot.val().make);
-      console.log(childSnapshot.val().model);
-      console.log(childSnapshot.val().zip);
-      console.log(childSnapshot.val().prices);
-
 
       $('#clunkerTable').append(
           "<tr><td id='yearDisplay'>" + childSnapshot.val().year +
@@ -72,73 +87,59 @@ dataRef.ref().on("child_added", function(childSnapshot) {
 
 
 
-var elt = document.getElementById("roller");
-var texts = ["!!$$$$$$!!", "--clunker-"]
-elt.textroller = new TextRoller({
-  el: elt,
-  values: texts, // an array of texts.     default : [el.innerHtml]
-  align: "middle", // right, left or middle. default : middle
-  delay: 3000, // in milliseconds,       default : 5000
-  loop: false // at the end, restart.   default : true
-});
-
-var els = document.getElementById("secRoll");
-var textz = ["!!$$$$$$!!", "connection"]
-els.textSecRoll = new TextRoller({
-  el: els,
-  values: textz, // an array of texts.     default : [el.innerHtml]
-  align: "middle", // right, left or middle. default : middle
-  delay: 3000, // in milliseconds,       default : 5000
-  loop: false // at the end, restart.   default : true
-});
 
 
-
-
-$(function() { //document onready function
+//~~~~~~~~~~~~~~~~~VEHICLE MAKE/MODEL DROP DOWN~~~~~~~~~~~~~~~~~~~//
 
 
 
 
 
 
-  let currentMake = "";
-  let currentModel = "";
-  let currentYear = "";
 
 
 
-  for (var i = 1950; i <= 2017; i++) {
+let vehicle = {
+  currentMake: "",
+  currentModel: "",
+  currentYear: "",
+  indexValue: "",
+  
+}
+
+//Builds Vehicle year dropdown
+  for (let i = 1950; i <= 2017; i++) {
       let makeString = "<option value ='" + i + "'>" + i + "</option>";
       $("#yearDataList").append(makeString);
 
   }
 
-  for (var i = 0; i < myData.length; i++) {
+//Builds vehicle Make drop down
+  for (let i = 0; i < myData.length; i++) {
       let makeString = "<option value ='" + i + "'>" + myData[i].title + "</option>";
       $("#carMakesDataList").append(makeString);
   }
 
 
+//Displays vehicle makes when year is selected
   $("#yearDataList").on('change', function() {
       $("#carMakesDataList").css('display', 'block');
-      currentYear = $(this).val();
+      vehicle.currentYear = $(this).val();
   })
 
 
 
 
-
+//Builds vehicle model drop down based on which vehicle make was selected
   $("#carMakesDataList").on('change', function() { //when make option is selected
-      $("#carModelsDataList").empty(); //empty model list everytime Make is selected
-      $("#carModelsDataList").append("<option value= '' disabled selected>Select Your Model</option>");
+      $("#carModelsDataList").empty(); //empty model list 
+      $("#carModelsDataList").append("<option value= '' disabled selected>Select Your Model</option>"); //Create placeholder
       $("#searchButton").css('display', 'none'); //hide search in case it's visible
-      indexValue = $(this).val(); //logs index number of object locatio
-      currentMake = myData[indexValue].title;
-      for (var i = 0; i < myData[indexValue].models.length; i++) { //creates options
+      vehicle.indexValue = $(this).val(); //logs index number in JSON object of vehicle Make 
+      vehicle.currentMake = myData[indexValue].title;
+      for (let i = 0; i < myData[indexValue].models.length; i++) { //creates options
           let makeString = "<option value ='" + myData[indexValue].models[i].title + "'>" + myData[indexValue].models[i].title + "</option>";
           $("#carModelsDataList").append(makeString);
-
       }
 
       $("#carModelsDataList").css('display', 'block'); //displays next selector area
@@ -148,14 +149,26 @@ $(function() { //document onready function
 
 
   $("#carModelsDataList").on('change', function() { //when model option is selected
-      currentModel = $(this).val();
+      vehicle.currentModel = $(this).val();
       $("#searchButton").css('display', 'block'); //displays search button
   });
 
 
 
 
-  var params = {
+
+
+
+
+//~~~~~~~~~~~~~~~~~ BING IMAGE API SEARCH~~~~~~~~~~~~~~~~~//
+
+
+
+
+
+
+
+  let params = {
       // Request parameters
       "q": "",
       "count": "10",
@@ -166,11 +179,10 @@ $(function() { //document onready function
 
 
 
-
-
   $("#searchButton").on('click', function() {
 
-      params.q = currentYear + " " + currentMake + " " + currentModel;
+      params.q = vehicle.currentYear + " " + vehicle.currentMake + " " + vehicle.currentModel;
+
       $.ajax({
 
 
@@ -195,4 +207,42 @@ $(function() { //document onready function
           });
   });
 
-})
+
+
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~TEXT ROLLER LOGO~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+
+
+
+
+
+let elt = document.getElementById("roller");
+let texts = ["!!$$$$$$!!", "--clunker-"]
+elt.textroller = new TextRoller({
+  el: elt,
+  values: texts, // an array of texts.     default : [el.innerHtml]
+  align: "middle", // right, left or middle. default : middle
+  delay: 2500, // in milliseconds,       default : 5000
+  loop: false // at the end, restart.   default : true
+});
+
+let els = document.getElementById("secRoll");
+let textz = ["!!$$$$$$!!", "connection"]
+els.textSecRoll = new TextRoller({
+  el: els,
+  values: textz, // an array of texts.     default : [el.innerHtml]
+  align: "middle", // right, left or middle. default : middle
+  delay: 2500, // in milliseconds,       default : 5000
+  loop: false // at the end, restart.   default : true
+});
+
+
+
+
+
+
+}) //end
+
